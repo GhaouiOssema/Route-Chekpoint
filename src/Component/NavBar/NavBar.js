f2b0af8d1257e1bf5bee384ce7a4be52a04bb3cd
oussema ghaoui <oghawi220@gmail.com>
2021-07-09 10:01:54 +0100

    new file

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import{Navbar ,Nav, }from 'react-bootstrap';
+import{Navbar }from 'react-bootstrap';
 import StarRatingComponent from "react-star-rating-component";
-import { NavLink   } from "react-router-dom";
 import "./Navbar.css"
 
 const NavBar = ({rate ,searchTerm,setRate,setSearchTerm,setHandleShow} ) => {
@@ -19,9 +18,7 @@ const NavBar = ({rate ,searchTerm,setRate,setSearchTerm,setHandleShow} ) => {
         <Navbar style={{background:'#ffcdd2',height:70}} variant="dark">
         <div className="form">        
             <div className="nav">
-            <div className="nav_link">
-            <NavLink to="/" className="link">Home</NavLink>
-            </div>
+                <img src="https://fabskill.com/assets/img/bus_cover/cropped/63_1565191317.webp" alt="Go My Code" style={{width : "10rem"}} />
             </div>
             <div className="starplusinput"> 
             <StarRatingComponent name="rate" className="reactstars" starCount={5}
